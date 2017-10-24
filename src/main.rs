e124f139e93286541dc90b149db1696dccec5bb3
MaxMEllon <maxmellon1994@gmail.com>
2017-10-24 18:21:31 +0900

    Implemented find

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 fn main() -> () {
-    let str = "hello, world";
-    println!("{}", str);
+    let list: Vec<u32> = vec![1, 2, 3, 4, 5];
+    fn f(v: &u32) -> bool { 3.eq(v) }
+    let func: fn(&u32) -> bool = f
+    let result = find::<u32>(&list, func).unwrap();
+    assert!(3.eq(result));
+}
+
+fn find<T>(xs: &Vec<T>, f: fn(&T) -> bool) -> Option<&T> {
+    // for x in xs {
+    //     if f(x) {
+    //         return Some(x);
+    //     }
+    // }
+    // return None
+    xs.iter().find(|&x| f(x))
 }
